02d3f81e0dbe50ba10a82c45f5fc80b9979e5aae
13geethika <109813734+13geethika@users.noreply.github.com>
2024-01-10 18:25:18 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import bgImg from './img1.jpg';
 import { useForm } from 'react-hook-form';
 import Navbar from './Navbar';
+import bgImg from './img1.jpg';
+import { useNavigate } from 'react-router-dom'; 
 import "./App.css";
 
-export default function Form() {
+const Form = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = async (data) => {
+  const navigate = useNavigate();
+
+  const handlebutton = async (data) => {
     try {
       const response = await fetch('http://localhost:5000/register', {
         method: 'POST',
@@ -18,6 +21,11 @@ export default function Form() {
 
       const result = await response.json();
       console.log(result.message);
+
+      if (result.message === 'OTP sent successfully') {
+        
+        navigate(`/otp?email=${data.Email}`); 
+      }
     } catch (error) {
       console.error('Error during registration:', error);
     }
@@ -31,13 +39,16 @@ export default function Form() {
           <h2>Sign In</h2>
           <span>register and enjoy the service</span>
 
-          <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("Email")} placeholder='email' />
-            <input type="password" {...register("Password")} placeholder='password' />
-            <input type="password" {...register("confirmpwd")} placeholder='confirm password' />
-            <input type="text" {...register("MobileNumber", { required: true, maxLength: 10 })} placeholder='mobile number' />
-            {errors.MobileNumber?.type === "required" && "Mobile Number is required"}
-            {errors.MobileNumber?.type === "maxLength" && "Max Length Exceed"}
+          <form id='form' className='flex flex-col' onSubmit={handleSubmit(handlebutton)}>
+            <input type="text" required {...register("Name")} placeholder='Name' />
+            <input type="email" required {...register("Email")} placeholder='Email' />
+            <input type="password" required {...register("Password")} placeholder='Password' />
+            <input type="password" required {...register("Confirmpassword")} placeholder='Confirm Password' />
+
+            {errors.Name?.type === "required" && "Name is required"}
+            {errors.Email?.type === "required" && "Email is required"}
+            {errors.Email?.type === "maxLength" && "Max Length Exceeded"}
+            {errors.Password?.type === "required" && "Password is required"}
             <button className='btn'>Sign In</button>
           </form>
         </div>
@@ -48,3 +59,5 @@ export default function Form() {
     </section>
   );
 }
+
+export default Form;
